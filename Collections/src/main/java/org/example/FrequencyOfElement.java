9bdb742fb7802f4779e77ea8b13ca3730370da87
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-02-11 12:39:43 +0530

    [Kapil Tiwari] Added frequencyOfElement file

---
+++ Collections/src/main/java/org/example/FrequencyOfElement.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FrequencyOfElement {
+
+    private static Map<String, Integer> countFrequency(List<String> fruits) {
+        Map<String,Integer> frequency = new HashMap<>();
+
+        for (String word : fruits){
+            frequency.put(word,frequency.getOrDefault(word,0)+1);
+        }
+        return frequency;
+    }
+
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("apple","banana","apple","orange");
+        Map<String,Integer> result = countFrequency(fruits);
+        System.out.println(result);
+    }
+}

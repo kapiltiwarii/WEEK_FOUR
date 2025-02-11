858464e3e7ac8af03a301072741e913d8c11fc3b
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-02-11 12:26:48 +0530

    [Kapil Tiwari] Added reverselist file]

---
+++ Collections/src/main/java/org/example/ReverseList.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.*;
+
+public class ReverseList {
+    // Method to reverse a List without built-in reverse method
+    public static <T> void reverseList(List<T> list) {
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            // Swap elements
+            T temp = list.get(left);
+            list.set(left, list.get(right));
+            list.set(right, temp);
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Using ArrayList
+        List<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        reverseList(arrayList);
+        System.out.println("Reversed ArrayList: " + arrayList);
+
+        // Using LinkedList
+        List<Integer> linkedList = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        reverseList(linkedList);
+        System.out.println("Reversed LinkedList: " + linkedList);
+    }
+}
+
